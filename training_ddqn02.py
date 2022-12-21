cdba339df7aaacab5e7370b5c562cf0ae52dac5c
Rafael Cunha <rafaelcunha2013@gmail.com>
2022-12-21 18:23:43 +0100

    generalized within platforms Linux, Windows, Mac

--- training_ddqn02.py
+++ training_ddqn02.py
@@ -5,17 +5,26 @@ from stable_baselines3 import DQN
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import CheckpointCallback, EvalCallback
 import os
+import platform
+from datetime import datetime
 from ddqn import DDQN
 
+
 model_name = "DDQN_optuna"
+unique_id = datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
 max_step = 500
 max_step_episode = 500
 render_mode = "rgb_array"
 env = gym.make("four-room-v0", render_mode=render_mode, max_episode_steps=max_step_episode)
 
 print('Start training')
-tensorboard_log = os.getcwd()
-# tensorboard_log = '/data/p285087/four_room/stable_baselines3' + '_' + model_name + '/'
+# Check in which system the code is running. Used to select the right path
+if platform.system() == 'Linux':
+    root = '/data/p285087/four_room/'
+else:
+    root = os.getcwd()
+tensorboard_log = os.path.join(root, 'stable_baselines3', model_name, unique_id)
+
 check_path = os.path.join(tensorboard_log, "logs/")
 
 checkpoint_callback = CheckpointCallback(save_freq=100*max_step, save_path=check_path, name_prefix="rl_model")
@@ -26,7 +35,11 @@ eval_callback = EvalCallback(eval_env, best_model_save_path=check_path,
 
 # Initialize the model
 model = DDQN("MlpPolicy", env, tensorboard_log=tensorboard_log, verbose=0)
-model.learn(total_timesteps=10_000_000, tb_log_name=model_name, callback=[checkpoint_callback, eval_callback], reset_num_timesteps=False, progress_bar=False)
+model.learn(total_timesteps=10_000_000,
+            tb_log_name=model_name,
+            callback=[checkpoint_callback, eval_callback],
+            reset_num_timesteps=False,
+            progress_bar=False)
 
 
 
