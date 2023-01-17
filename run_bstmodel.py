cd68bd9ddb808dec72aaab324114b51c45273f89
Rafael Cunha <rafaelcunha2013@gmail.com>
2023-01-17 11:19:19 +0100

    new video path and multiagent environment

--- run_bstmodel.py
+++ run_bstmodel.py
@@ -2,6 +2,7 @@ import gym
 import gym_sf
 
 from stable_baselines3 import DQN
+from ddqn import DDQN
 import os
 
 import time
@@ -25,11 +26,22 @@ def my_test(model, env_test):
 
 max_step = 100
 render_mode = "rgb_array"
-env = gym.make("four-room-v0", render_mode=render_mode, max_episode_steps=max_step)
+env = gym.make("four-room-multiagent-v0",
+               render_mode=render_mode,
+               max_episode_steps=500,
+               random_initial_position=False)
 
-path = os.path.join(os.getcwd(), "Peregrine", "DQN_03", "models/best_model.zip")
-model = DQN.load(path)
-env_test = gym.make("four-room-v0", render_mode="rgb_array_list", max_episode_steps=max_step, video=True, random_initial_position=True)
+# path = os.path.join(os.getcwd(), "Peregrine", "DQN_03", "models/best_model.zip")
+path = os.path.join(os.getcwd(), "stable_baselines3", "DDQN", "2023_01_16__19_37_45", "logs/best_model.zip")
+print(path)
+video_path = os.path.join(os.getcwd(), "stable_baselines3", "DDQN", "2023_01_16__19_37_45")
+model = DDQN.load(path)
+env_test = gym.make("four-room-multiagent-v0",
+                    render_mode="rgb_array_list",
+                    max_episode_steps=max_step,
+                    video=True,
+                    random_initial_position=False,
+                    video_path=video_path)
 
 for _ in range(3):
     my_test(model, env_test)
